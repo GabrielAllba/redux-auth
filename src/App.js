d54983aeca4593eedd4055df74fc0aa7eceb5680
rielgbrl <rielgasy9@gmail.com>
2023-01-18 08:14:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Counter from './components/Counter';
+import { Fragment } from 'react';
+import Header from './components/Header'
+import Auth from './components/Auth'
+function App() {
+  return (
+    <Fragment>
+      <Header></Header>
+      <Auth></Auth>
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
